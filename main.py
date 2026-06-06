152413c9a1af0f73bee364b353c72339b804f650
Obj3c-t <mak97799@gmail.com>
2026-06-05 22:34:12 -0600

    Create main.py
    VERY Basic barebones code, experimenting with pygame as I am rusty.

---
+++ main.py
@@ -0,0 +1,75 @@
+import pygame
+import sys
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Platformer")
+
+clock = pygame.time.Clock()
+
+TILE_SIZE = 32
+
+game_map = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    # Floor starts here (Row 12 out of 18)
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], 
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+]
+
+player_x = 400
+player_y = 300
+player_speed = 5
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+	keys = pygame.key.get_pressed()
+	if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+		player_x -= player_speed
+	if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+		player_x += player_speed
+	if keys[pygame.K_UP] or keys[pygame.K_w]:
+		player_y -= player_speed
+	if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+		player_y += player_speed
+
+	tile_rects = []
+
+	for row_index, row in enumerate(game_map):
+		for col_index, tile in enumerate(row):
+			if tile == 1:
+				block_x = col_index * TILE_SIZE
+				block_y = row_index * TILE_SIZE
+
+
+				tile_rects.append(pygame.Rect(block_x, block_y, TILE_SIZE, TILE_SIZE))
+	
+	screen.fill((135, 206, 235))
+
+	for tile in tile_rects:
+		pygame.draw.rect(screen, (34, 139, 34), tile)
+	
+	pygame.draw.rect(screen, (255,0,0), (player_x, player_y, 32, 32))
+
+	pygame.display.flip()
+	clock.tick(60)
